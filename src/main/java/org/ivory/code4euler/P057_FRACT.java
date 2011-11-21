8f437e6478b9e9cd898a0eb892695ca60854804b
ivoryxiong <xiong4foss@gmail.com>
2011-11-21 22:02:47 +0800

    ADD: solved problem 57

---
+++ src/main/java/org/ivory/code4euler/P057_FRACT.java
@@ -0,0 +1,29 @@
+package org.ivory.code4euler;
+
+import java.math.BigInteger;
+
+public class P057_FRACT extends EasyEuler {
+	int cnt = 0;
+
+	public int run(int k) {
+		int res = 0;
+		BigInteger n1 = new BigInteger("1");
+		BigInteger n2 = new BigInteger("1");
+		for (int i = 1; i <= k; i++) {
+			n1 = n1.add(n2).add(n2);
+			n2 = n1.subtract(n2);
+			if (n1.toString().length() > n2.toString().length()) {
+				res++;
+			}
+		}
+		return res;
+	}
+
+	public static void main(String[] args) {
+		long beg = System.currentTimeMillis();
+		P057_FRACT p57 = new P057_FRACT();
+		System.out.println(p57.run(1000));
+		long time = System.currentTimeMillis() - beg;
+		System.out.println(time + " ms");
+	}
+}
